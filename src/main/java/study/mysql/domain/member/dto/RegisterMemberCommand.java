15ab814cb606990a22db6656e1520ae5d71c844d
haemil <every.haemil@gmail.com>
2023-09-05 16:04:44 +0900

    add docs

--- src/main/java/study/mysql/domain/member/dto/RegisterMemberCommand.java
+++ src/main/java/study/mysql/domain/member/dto/RegisterMemberCommand.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
     record
     - Getter, Setter 자동생성.
     - 생성된 getter, setter 를 property 형식으로 사용할 수 있음.
+    - Java의 Record class 를 상속받는 형태로 구현되어 있음.
  */
 public record RegisterMemberCommand(
         String email,
